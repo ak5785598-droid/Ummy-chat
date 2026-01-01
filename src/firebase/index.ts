276f536fff3f43d1a305f3f1084a90b244d6cf7d
Anshu Kumar <ak5785598@gmail.com>
2026-01-01 17:20:26 +0000

    not working please do something

--- src/firebase/index.ts
+++ src/firebase/index.ts
@@ -1,3 +1,4 @@
+
 'use client';
 
 import { firebaseConfig } from '@/firebase/config';
@@ -26,7 +27,5 @@ export * from './provider';
 export * from './firestore/use-collection';
 export * from './firestore/use-doc';
 export * from './auth/use-user';
-export * from './non-blocking-updates';
-export * from './non-blocking-login';
 export * from './errors';
 export * from './error-emitter';
