aa50aa2565a0888fe24691b4e7dd832395ba1b85
Ansh Jha <ak5785598@gmail.com>
2026-04-16 13:12:14 +0530

    fix: Register FirebaseAuthenticationPlugin in MainActivity

--- android/app/src/main/java/app/vercel/ummy_chat/twa/MainActivity.java
+++ android/app/src/main/java/app/vercel/ummy_chat/twa/MainActivity.java
@@ -2,10 +2,13 @@ package app.vercel.ummy_chat.twa;
 
 import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
+import io.capawesome.capacitor.firebase.auth.FirebaseAuthenticationPlugin;
+
 public class MainActivity extends BridgeActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        registerPlugin(FirebaseAuthenticationPlugin.class);
         registerPlugin(AudioRoutePlugin.class);
     }
 }
