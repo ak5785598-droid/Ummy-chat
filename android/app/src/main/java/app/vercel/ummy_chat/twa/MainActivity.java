ab617afe8318b7067dd54771c9b8fe95e750945f
Ansh Jha <ak5785598@gmail.com>
2026-04-16 15:52:07 +0530

    debug: Added app name change and Toast for visual verification of APK update

--- android/app/src/main/java/app/vercel/ummy_chat/twa/MainActivity.java
+++ android/app/src/main/java/app/vercel/ummy_chat/twa/MainActivity.java
@@ -2,6 +2,7 @@ package app.vercel.ummy_chat.twa;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Toast;
 import com.getcapacitor.BridgeActivity;
 import com.google.firebase.FirebaseApp;
 import io.capawesome.capacitorjs.plugins.firebase.authentication.FirebaseAuthenticationPlugin;
@@ -15,6 +16,7 @@ public class MainActivity extends BridgeActivity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Log.i("UmmyAuth", "=== MainActivity Start ===");
+        Toast.makeText(this, "NATIVE BRIDGE READY", Toast.LENGTH_LONG).show();
         try {
             Log.i("UmmyAuth", "Initializing Firebase App...");
             FirebaseApp.initializeApp(this);
