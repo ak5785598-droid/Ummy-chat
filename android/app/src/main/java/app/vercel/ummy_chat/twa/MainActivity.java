c7b029b814078b9c12a0b3ff67b894857b0e9ec1
Ansh Jha <ak5785598@gmail.com>
2026-04-16 14:24:28 +0530

    fix: Robust native Firebase initialization and version alignment

--- android/app/src/main/java/app/vercel/ummy_chat/twa/MainActivity.java
+++ android/app/src/main/java/app/vercel/ummy_chat/twa/MainActivity.java
@@ -1,11 +1,15 @@
 package app.vercel.ummy_chat.twa;
 
 import android.os.Bundle;
+import android.util.Log;
 import com.getcapacitor.BridgeActivity;
+import com.google.firebase.FirebaseApp;
 
 public class MainActivity extends BridgeActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d("UmmyAuth", "Initializing Firebase in MainActivity");
+        FirebaseApp.initializeApp(this);
     }
 }
